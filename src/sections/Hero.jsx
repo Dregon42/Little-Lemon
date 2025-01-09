9e9b241f0ef9e3b8a7c993cb5c3f8b019faec2d8
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-09 12:45:00 -0500

    intitial hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <section>
-      <div id='hero-textContainer'>
+    <section className='hero-section' >
+      <div className='hero-textContainer'>
         <h1>Little Lemon</h1>
         <h3>Chicago</h3>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-             Nobis consequuntur atque ducimus sint qui aspernatur nostrum reprehenderit, 
-             omnis ipsa veritatis id? Quisquam, cupiditate ullam.
+        <p className='hero-subtext'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Nobis consequuntur atque ducimus sint qui aspernatur nostrum reprehenderit, 
+            omnis ipsa veritatis id? Quisquam, cupiditate ullam.
         </p>
+        <button  type="button">Reserve a Table</button>
       </div>
-      <div id='image-container'>
-        <img src="" alt="" />
+      <div className='image-container'>
+        <img className='hero-image' src="src/assets/Grilled_Fish.jpg" alt="Image of fish being grilled" />
       </div>
     </section>
   )
