9734e0ec5f2e9b2a376e6dd7b6e2146c97335591
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-09 11:22:56 -0500

    update special cards

--- src/components/SpecialCard.jsx
+++ src/components/SpecialCard.jsx
@@ -5,17 +5,19 @@ const SpecialCard = ({special}) => {
   return (
     <figure key={special.name} className="special-card">
       <img className='special-image' src={special.image} alt={special.name} />
-      <figcaption className="special-details">
-        <div className='dish-header'>
-          <h3 className='special-name'>{special.name}</h3>
-          <p className="special-price">{special.price}</p>
-        </div>
-        <p className='special-description'>{special.description}</p>
-      </figcaption>
-      <button type='btn' className='delivery-button'>
-        <p>Order a Delivery</p>
-        <img className='delivery-icon' src="src/assets/delivery.png" alt="Delivery Truck" />
-      </button>
+      <div className='special-content'>
+        <figcaption className="special-details">
+          <div className='dish-header'>
+            <h3 className='special-name'>{special.name}</h3>
+            <p className="special-price">{special.price}</p>
+          </div>
+          <p className='special-description'>{special.description}</p>
+        </figcaption>
+        <button type='btn' className='delivery-button'>
+          <p>Order a Delivery</p>
+          <img className='delivery-icon' src="src/assets/delivery.png" alt="Delivery Truck" />
+        </button>
+      </div>
     </figure>
   )  
 }
--- src/testData.js
+++ src/testData.js
@@ -4,19 +4,19 @@ export const specials = [
         name: 'Brischetta',
         image: 'src/assets/Brischetta.jpg',
         price: '$12.99',
-        description: 'Fresh, flavorful bruschetta: a perfect bite of Italian-inspired delight!'
+        description: 'Savor the perfect blend of crispy bread, ripe tomatoes, fresh basil, and garlic—bruschetta brings bold, authentic Italian flavors to life!'
     },
     {
         name: 'Penne Pasta',
         image: 'src/assets/Pasta.jpg',
         price: '$15.99',
-        description: 'Deliciously versatile penne pasta: perfect for every sauce and occasion!'
+        description: 'Indulge in penne pasta, the perfect companion to rich sauces and vibrant ingredients for a delicious Italian dining experience!'
     },
     {
         name: 'Grilled Flounder',
         image: 'src/assets/entree.jpg',
         price: '$24.99',
-        description: 'Succulent grilled fish: healthy, flavorful, and perfectly charred goodness!"'
+        description: 'Experience the fresh, flavorful taste of perfectly grilled fish, seasoned with Italian herbs, drizzled with olive oil, and paired with vibrant Mediterranean-inspired sides. Deliciously unforgettable!'
     },
 ];
 
