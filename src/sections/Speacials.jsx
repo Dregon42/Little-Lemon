3094055b7ef7e5aed634350bc6bae11425331b25
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-10 14:37:32 -0500

    Update to specials section

--- src/sections/Speacials.jsx
+++ src/sections/Speacials.jsx
@@ -6,12 +6,19 @@ import './Specials.css';
 const Speacials = () => {
   return (
     <section className='specials-section'>
-    
-      {
-        specials.map((special) => (
-          <SpecialCard special={special} />
-        ))
-      }
+      <div className='specials-container'>
+        <div className='special-text'>
+          <h3>Specials</h3>
+          <button type="button">On the Menu</button>
+        </div>
+        <div className='cards-container'>
+          {
+            specials.map((special) => (
+                <SpecialCard special={special} key={special.name} />
+            ))
+          }
+        </div>
+      </div>
     
     </section>
   )
