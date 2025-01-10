c5047e933dcb21359fe8d16864fa5e36f790776b
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-10 14:36:36 -0500

    initial highlights section

---
+++ src/sections/Highlights.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Highlights.css';
+
+const Highlights = () => {
+  return (
+    <section className='highlights-container'>
+      <div className='highlights-textContainer'>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p className='highlight-subtext'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Nobis consequuntur atque ducimus sint qui aspernatur nostrum reprehenderit, 
+            omnis ipsa veritatis id? Quisquam, cupiditate ullam.
+        </p>
+        <div className='circle'></div>
+      </div>
+      <div className='highlightImage-container'>
+        <div className='highlight-box'></div>
+        <img className='highlight-image' src="src/assets/Grilled_Fish.jpg" alt="Grilled Fish" />
+      </div>
+    </section>
+  )
+}
+
+export default Highlights
