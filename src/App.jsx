474142e2aab3c133a9897f8a8b425247f506e95c
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-21 00:04:50 -0500

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/nav/Navbar";
 import { Home } from "./pages/Home";
+import { Reservation } from "./pages/Reservation";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 
 function App() {
  
 
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reservation" element={<Reservation />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
