1029232dcf2fca342bc3a19016e117fdff2d96fe
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-23 21:17:42 -0500

    Yup validation

--- src/components/form/ReservationForm.jsx
+++ src/components/form/ReservationForm.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import './ReservationForm.css';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import ConfirmationModal from '../MUI/ConfirmationModal';
+import { data } from 'react-router';
+
+const schema = yup.object({
+    fullName: yup.string().required(),
+    phone: yup.string().required(),
+    email: yup.string().required().email(),
+    // date: yup.date().required().min(new Date(2025, 0, 27)).max(new Date(2025, 1, 1).e),
+    party: yup.number().positive().required(),
+    occassion: yup.string().notRequired(),
+    seating: yup.string().notRequired()
+}).required()
+
+let reservation;
 
 export const ReservationForm = () => {
-    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm({ resolver: yupResolver(schema)});
+
 
     const [open, setOpen] = useState(false);
 
@@ -17,23 +33,25 @@ export const ReservationForm = () => {
     };
 
     const onSubmit = (data) => {
-        console.log(data)
+
+        reservation = data
+        console.log(reservation)
     }
 
     
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
             <div className='form-container'>
                 <div>
-                    <label htmlFor="fullName" autoFocus >Name *</label>
+                    <label htmlFor="fullName" autoFocus >Name*</label>
                     <input 
                         type="text" 
                         name='fullName'
                         id='fullName' 
                         aria-required="true" 
                         placeholder='Enter name'
-                        {...register('fullName', { required: 'This field required' })}
+                        {...register('fullName', { required: 'Your name is required' })}
                         aria-invalid={errors.fullName ? 'true' : 'false'}
                     />
                     {errors.fullName && <span role='alert'>{errors.fullName.message}</span>}
@@ -45,10 +63,13 @@ export const ReservationForm = () => {
                         id='phone' 
                         name='phone'
                         placeholder='Enter number'
-                        {...register('phone', {required: 'This field required', minLength: 10})}
+                        {...register('phone', {
+                            required: 'Phone number is required', 
+                            minLength: 10
+                        })}
                         aria-invalid={errors.phone ? 'true' : 'false'}
                     />
-                    {errors.phone && <span role='alert'>{errors.phone.message}</span>}
+                    <span role='alert'>{errors.phone?.message}</span>
                 </div>
             </div>
             <div className='form-container'>
@@ -60,10 +81,16 @@ export const ReservationForm = () => {
                         name='email'
                         aria-required="true" 
                         placeholder='Enter email'
-                        {...register('email', { required: 'This field required'})}
+                        {...register('email', { required: 'Email Required',
+                            pattern: {
+                                value:  /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/,
+                                message: 'Invalid email'
+                            }
+
+                        })}
                         aria-invalid={errors.email ? 'true' : 'false'}
                     />
-                    {errors.email && <span role='alert'>{errors.email.message}</span>}
+                    <span role='alert'>{errors.email?.message}</span>
                 </div>
                 <div>
                     <label htmlFor="date">Date</label>
@@ -73,10 +100,22 @@ export const ReservationForm = () => {
                         name='date'
                         aria-required="true" 
                         placeholder='MM/DD/YYYY'
-                        {...register('date', {required: 'Required'})}
+                        {...register('date', {
+                            required: 'Required',
+                            validate: {
+                                inRange: (day) => {
+                                    const selectedDate = new Date(day);
+                                    const minDate = new Date("01-27-2025");
+                                    const maxDate = new Date("02-01-2025");
+                                    return selectedDate >= minDate && selectedDate <= maxDate || 
+                                        'Date must be between 2025-01-27 and 2025-02-01';
+                                }
+                            }
+                            
+                        })}
                         aria-invalid={errors.date ? 'true' : 'false'}
                     />
-                    {errors.date && <span role='alert'>{errors.date.message}</span>}
+                    <span role='alert'>{errors.date?.message}</span>
                 </div>
             </div>
             <div className='form-container'>
@@ -119,7 +158,7 @@ export const ReservationForm = () => {
                     </select>
                 </div>
             </div>
-            <ConfirmationModal  open={open} handleOpen={handleOpen} handleClose={handleClose} />
+            <ConfirmationModal reservation={reservation}  open={open} handleOpen={handleOpen} handleClose={handleClose} />
         </form>
     )
 };
