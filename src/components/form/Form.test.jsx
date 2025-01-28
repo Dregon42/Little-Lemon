fa97fc1213a379c9a6fc64508870dcc3e0a60366
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-28 11:35:17 -0500

    Error validation text

--- src/components/form/Form.test.jsx
+++ src/components/form/Form.test.jsx
@@ -1,15 +1,41 @@
-import {  it, test, vi } from "vitest";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { expect, it, vi } from "vitest";
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import { ReservationForm } from './ReservationForm.jsx';
 
 describe('Reservation Form', () => {
-  test('completed form', () => {
-    render(<ReservationForm title={'Form'}/>)
+  it('displays error validations', async () => {
+    render(<ReservationForm />);
 
-    expect(screen.getByLabelText('Name*')).toBeInTheDocument();
-    expect(screen.getByLabelText('Phone Number*')).toBeInTheDocument();
-    expect(screen.getByLabelText('Email*')).toBeInTheDocument();
-    expect(screen.getByLabelText('Date*')).toBeInTheDocument();
-    expect(screen.getByLabelText('Party')).toBeInTheDocument();
-  })
-})
+    const user = userEvent.setup();
+
+    const testName = 'Rae Babineaux';
+    const testPhone = '123-456-7890';
+    const testEmail = 'test2test.com'; // Invalid email
+    const testDate = '2025-01-28';
+    const testParty = 5;
+
+    const nameInput = await screen.findByLabelText('Name*');
+    const phoneInput = await screen.findByLabelText('Phone Number*');
+    const emailInput = await screen.findByLabelText('Email*');
+    const dateInput = await screen.findByLabelText('Date*');
+    const partyInput = await screen.findByLabelText('Party');
+
+    await user.type(nameInput, testName);
+    await user.type(phoneInput, testPhone);
+    await user.type(emailInput, testEmail);
+    await user.type(dateInput, testDate);
+    await user.type(partyInput, testParty.toString());
+
+    expect(nameInput).toHaveValue(testName);
+    expect(phoneInput).toHaveValue(testPhone);
+    expect(emailInput).toHaveValue(testEmail);
+    expect(dateInput).toHaveValue(testDate);
+    expect(partyInput).toHaveValue(testParty);
+
+    const button = screen.getByRole('button', { name: /reserve/i });
+    await user.click(button);
+
+    expect(screen.getByText('email must be a valid email'));
+  });
+});
