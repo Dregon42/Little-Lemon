2b59ea123834e79f23168bbe2239c4a540f9f102
L'Rae Brantley <lbrantl13@gmail.com>
2025-01-28 18:32:00 -0500

    mobile nav styling

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    
+
   return (
     <nav>
-        <div>
-            <Link to={'/'}>
-                <img className='logo' src='src/assets/LL_Logo.jpg' alt="Little Lemon Logo" />
-            </Link>
-        </div>
-        <ul className='nav-list'>
-            <li className='nav-item' >
-                <a href="#">About</a>
-            </li>
-            <li className='nav-item' >
-                <a href="#">Reservation</a>
-            </li>
-            <li className='nav-item' >
-                <a href="#">Order Online</a>
-            </li>
-            <li className='nav-item' >
-                <a href="#">Login</a>
-            </li>
-        </ul>
+        
+        <Link to={'/'} className='logo-container'>
+            <img className='logo' src='src/assets/LL_Logo.jpg' alt="Little Lemon Logo" />
+        </Link>
+
+        <button className='menu-button' onClick={() => setMenuOpen(!menuOpen)}>
+            {
+                !menuOpen ?
+                (
+
+                    <FontAwesomeIcon icon={faBars} />
+                )
+                :
+                (
+                    <FontAwesomeIcon icon={faXmark}/>
+                )
+            }
+        </button>
+        {
+            menuOpen ? (
+                <ul className='nav-list'>
+                    <li className='nav-item' >
+                        <a href="#">About</a>
+                    </li>
+                    <li className='nav-item' >
+                        <a href="#">Reservation</a>
+                    </li>
+                    <li className='nav-item' >
+                        <a href="#">Order Online</a>
+                    </li>
+                    <li className='nav-item' >
+                        <a href="#">Login</a>
+                    </li>
+                </ul>
+
+            ) 
+            :
+            null
+        }
     </nav>
   )
 }
